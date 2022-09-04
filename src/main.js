695de77b95a6691fe3cada86da8fac6401b20e34
Broseph Stylin <yuri_csilva@hotmail.com>
2022-09-04 20:33:28 -0300

    Adds new app file

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
-import App from "./App.vue";
+import AppNew from "./App-new.vue";
 
 Vue.config.productionTip = false;
 
 new Vue({
-	render: h => h(App),
+	render: h => h(AppNew),
 }).$mount("#app");
